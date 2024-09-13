2046ba327c050c618acbb4cf12345ed50eccf7ac
kavishka-sandeepa-97 <kavishkapk99@gmail.com>
2024-09-13 22:34:31 +0530

    create express app

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express=require('express');
 const app=express();
-const cors=require('cors');
+const cors=require('cors'); //cros origin resource sharing
+
+app.use(cors());
+app.use(express.urlencoded({
+    extended:true
+})
+);
+app.use(express.jason()); //all data transfer in jason
+
+module.exports=app;
